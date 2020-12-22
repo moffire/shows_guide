5b8fa85c7a233d17cb399207ae93a0f30630df74
moffire <alexeydudarev.spb@gmail.com>
2020-12-22 20:53:21 +0300

    Update urls

--- shows/urls.py
+++ shows/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MainView, MovieDetail
+from .views import MainView, MovieDetail, SearchView, RatingView
 
 app_name = 'shows'
 
 urlpatterns = [
-    path('', MainView.as_view(), name='main_page'),
-    path('<str:rating>/', MainView.as_view(), name='main_page'),
-    path('<int:external_id>/', MovieDetail.as_view(), name='detail'),
+    path('', MainView.as_view(), name='index'),
+    path('<str:rating>/', RatingView.as_view(), name='rating'),
+    path('details/<int:external_id>/', MovieDetail.as_view(), name='detail'),
+    path('search', SearchView.as_view(), name='search'),
 ]
